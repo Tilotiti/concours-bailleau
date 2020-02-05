5040d00e85994ac24a7a7e5265bac5de934b28ff
Thibault HENRY <thibault@fidcar.com>
2020-02-05 01:40:04 +0100

    Thanks

---
+++ src/Entity/Thank.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+
+/**
+ * Class Thank
+ * @package App\Entity
+ * @ORM\Entity()
+ */
+class Thank implements TranslatableInterface
+{
+    use TranslatableTrait;
+
+    /**
+     * @var integer
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+}
---
+++ src/Entity/ThankTranslation.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslationInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ThankTranslation implements TranslationInterface
+{
+    use TranslationTrait;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     */
+    private $label;
+
+    /**
+     * @return string|null
+     */
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param string|null $label
+     */
+    public function setLabel(?string $label): void
+    {
+        $this->label = $label;
+    }
+}
